7da1d32ae2f39372de125de4765bc05878e3cdca
KarimLeon <leon.karim08@gmail.com>
2015-12-10 00:44:41 -0500

    More Comments

--- LoopTriangle.js
+++ LoopTriangle.js
@@ -1,6 +1,6 @@
 var y = "" //Created a variable, then gave it a empty string value which equals the Boolean Value "False"!
 
-for(var x = 1; x <= 7; x++) {  
+for(var x = 1; x <= 7; x++) {  //Variable X Initialized the loop, 2nd statement checks whether it should keep going so if it's true, it'll move on, and 3rd statment increments x by 1.
 
     y += "#"
 	
