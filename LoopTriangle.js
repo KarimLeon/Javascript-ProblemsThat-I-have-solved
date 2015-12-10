eac09ff22cc7a4976a16234a5efa43af0b24ec98
KarimLeon <leon.karim08@gmail.com>
2015-12-10 00:39:34 -0500

    Little fix

--- LoopTriangle.js
+++ LoopTriangle.js
@@ -1,4 +1,4 @@
-var y = "" /* Created a variable, then gave it a empty string value which equals the Boolean Value "False"!*/
+var y = "" \\Created a variable, then gave it a empty string value which equals the Boolean Value "False"!
 
 for(var x = 1; x <= 7; x++) {  
 
