43b78756d6eb6054b8ef61d7ac405d0de699be84
KarimLeon <leon.karim08@gmail.com>
2017-04-28 15:23:16 -0400

    Hack-Rank Practice... 'Rank.js'

---
+++ rank.js
@@ -0,0 +1,22 @@
+function processData(input) {
+    var array = [], arr1 = []
+
+ for(var i = 2; i < input.length; i++) {
+      array.push(input[i]);
+
+};
+
+
+for(var b = 0; b < array.length; b++) {
+         if(array[b] != '\n') 
+            arr1.push(array[b]);
+        else
+          arr1.push([[arr1[b]]]);
+
+
+        
+
+}
+
+console.log(arr1);
+};
