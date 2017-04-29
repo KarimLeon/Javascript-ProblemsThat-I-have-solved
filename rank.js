74ece5129d44cb31f85128c9ffc36cbc3e6f6f83
KarimLeon <leon.karim08@gmail.com>
2017-04-29 12:11:03 -0400

    'change'

--- rank.js
+++ rank.js
@@ -9,6 +9,27 @@ function processData(input) {
 
 for(var b = 0; b < array.length; b++) {
          if(array[b] != '\n') 
+            arr1.push(array2);
+        else
+            break;
+
+        
+array2.push(array[b])
+}
+
+
+console.log(arr1);
+};
+    var array = [], arr1 = [], array2 = []
+
+ for(var i = 2; i < input.length; i++) {
+      array.push(input[i]);
+
+};
+
+
+for(var b = 0; b < array.length; b++) {
+         if(array[b] != '\n') 
             arr1.push(array2.push(array[b]));
         else
             break; 
