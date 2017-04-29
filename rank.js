2f990cc5ab433fa3d7c6e3ffa11ba946b01360c1
KarimLeon <leon.karim08@gmail.com>
2017-04-29 12:39:26 -0400

    Update Again..

--- rank.js
+++ rank.js
@@ -1,23 +1,14 @@
 function processData(input) {
-    var array = [], arr1 = [], array2 = []
+    var array = [], arr1 = [], array2 = [], y = ""
 
  for(var i = 2; i < input.length; i++) {
       array.push(input[i]);
 
 };
 
-
-for(var b = 0; b < array.length; b++) {
-         if(array[b] != '\n') 
-            arr1.push(array2);
-        else
-            break;
-
-        
-array2.push(array[b])
+for(var i = 0; i < array.length; i++) {
+      var a  = array.join("")
 }
 
-
-console.log(arr1);
+console.log(a)
 };
-   
