8c408f32f8364f0a046622b053c3731af201f5c7
KarimLeon <leon.karim08@gmail.com>
2017-04-29 12:16:38 -0400

    Fixed

--- rank.js
+++ rank.js
@@ -20,27 +20,4 @@ array2.push(array[b])
 
 console.log(arr1);
 };
-    var array = [], arr1 = [], array2 = []
-
- for(var i = 2; i < input.length; i++) {
-      array.push(input[i]);
-
-};
-
-
-for(var b = 0; b < array.length; b++) {
-         if(array[b] != '\n') 
-            arr1.push(array2.push(array[b]));
-        else
-            break; 
-            
-
-
-        
-
-}
-
-console.log(arr1);
-};
-
-s
+   
