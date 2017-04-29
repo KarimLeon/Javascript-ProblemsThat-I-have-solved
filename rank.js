40e5800099d0cf8e77fcd8adebc571621398802d
KarimLeon <leon.karim08@gmail.com>
2017-04-29 13:14:54 -0400

    LOL

--- rank.js
+++ rank.js
@@ -1,5 +1,5 @@
 function processData(input) {
-    var array = [], arr1 = [], array2 = [], y = ""
+    var array = [], arr1 = [], array2 = []
 
  for(var i = 2; i < input.length; i++) {
       array.push(input[i]);
@@ -9,11 +9,14 @@ function processData(input) {
 for(var i = 0; i < array.length; i++) {
      array2.push(array[i]);
 
-     if(array2[i] )
+     if(array2[i] != '\n') 
+            arr1.push(array2[i]);
+     else 
+          break;
       
      
 }
 
+console.log(arr1);
 
-console.log(array2)
 };
