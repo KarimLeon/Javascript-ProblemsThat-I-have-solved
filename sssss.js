ddccbc75fa69a6208216e4784d5c24b5c3fd1415
KarimLeon <leon.karim08@gmail.com>
2017-04-30 23:32:13 -0400

    x

---
+++ sssss.js
@@ -0,0 +1,12 @@
+var size = 4, y = "", b = ""
+
+for(var x = 0; x < size; x++) {
+        if(y.length == 4)
+             b+= '\n';
+           
+for(var i = 0; i < size; i++) { 
+       y += " #";
+};
+}; 
+
+console.log(y);
