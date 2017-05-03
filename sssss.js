14d6003ba05a9f3dae00355ce99f8b157621e389
KarimLeon <leon.karim08@gmail.com>
2017-05-03 18:36:38 -0400

    chenge

--- sssss.js
+++ sssss.js
@@ -1,7 +1,6 @@
-var y = '', counter = 0, size = 8
+var y = '',  size = 8
 
 for(var x = 0; x < size; x++) {
-         if(size / 2)
             y += '\n'
    for(var i = 0; i < size; i+=2) {
          y += ' #';
@@ -10,3 +9,4 @@ for(var x = 0; x < size; x++) {
 }; 
 };
 console.log(y)
+
