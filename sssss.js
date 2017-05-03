af52377d51deb3e82e505abb7f900bca488ef039
KarimLeon <leon.karim08@gmail.com>
2017-05-03 18:30:47 -0400

    yuuuup

--- sssss.js
+++ sssss.js
@@ -1,15 +1,12 @@
 var y = '', counter = 0, size = 8
 
-for(var i = 0; i < size; i++) {
-      
-if(size % 2)
-          y += '\n'
-for(var i = 0; i < size; i++) {
-      y+= ' #'
+for(var x = 0; x < size; x++) {
+         if(size / 2)
+            y += '\n'
+   for(var i = 0; i < size; i+=2) {
+         y += ' #';
     
           
 }; 
 };
-
-console.log(y);
-
+console.log(y)
