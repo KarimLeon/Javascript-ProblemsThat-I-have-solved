98d9255d454b963a143509f3edbff74466ced283
KarimLeon <leon.karim08@gmail.com>
2017-05-03 21:55:00 -0400

    Edited

--- sssss.js
+++ sssss.js
@@ -1,6 +1,9 @@
 var y = '',  size = 8
 
 for(var x = 0; x < size; x++) {
+         if(y == "") 
+		      null; 
+		 else 
             y += '\n'
    for(var i = 0; i < size; i+=2) {
          y += ' #';
