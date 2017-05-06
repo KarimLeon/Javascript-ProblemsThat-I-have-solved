45ce2250da408447cd3f69b190391900d81cc68a
KarimLeon <leon.karim08@gmail.com>
2017-05-06 15:14:45 -0400

    update

--- sssss.js
+++ sssss.js
@@ -1,14 +1,17 @@
-var y = "", a = ""
+var y = "", a = "", size = 8
 
-for(var i = 0; i < 8; i++) {
-       
-           y += '\n'
-            
-  for(var e = 0; e < 8; e+=2) {
-         y += ' #';
+for(var i = 0; i < size; i++) {
+         if(y.length == size)
+             y += '\n'
+          else
+             y += ' # '
+          
+  for(var e = 0; e < size; e+=3) {
+         y += '# ';
 
         
   }
 }; 
 
 console.log(y)
+
