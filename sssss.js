2e4d8a423b180db4d54d973420e6254eab9dcad8
KarimLeon <leon.karim08@gmail.com>
2017-05-06 17:36:14 -0400

    'chenge'

--- sssss.js
+++ sssss.js
@@ -15,3 +15,24 @@ for(var i = 0; i < size; i++) {
 
 console.log(y)
 
+
+/*
+var y = "", a = [], size = 8
+
+for(var i = 0; i < size + 1; i+=1) {
+          
+    y += '\n'
+
+  
+   
+  for(var e = 0; e < size + 1; e+=2) {
+         y +=  '#';
+         
+
+        
+  }
+}; 
+a.push(y);
+
+console.log(a)
+*/
