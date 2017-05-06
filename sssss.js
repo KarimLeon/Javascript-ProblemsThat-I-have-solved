8ed19fc38af3608984d55413f237a66d1c9c38b6
KarimLeon <leon.karim08@gmail.com>
2017-05-06 02:34:02 -0400

    Change

--- sssss.js
+++ sssss.js
@@ -1,14 +1,14 @@
-
 var y = "", a = ""
 
 for(var i = 0; i < 8; i++) {
-       if(y == 8)
+       
            y += '\n'
             
   for(var e = 0; e < 8; e+=2) {
          y += ' #';
+
+        
   }
 }; 
 
 console.log(y)
-       
